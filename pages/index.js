4ba7cc9d05941a7aacbf18a2c6c2a3e1e63797c3
AmmarMasood <ammar.masood98@gmail.com>
2023-03-28 00:08:57 +0500

    new changes

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   const [inputValue, setInputValue] = useState("");
-  const [showSelector, setShowSelector] = useState(false);
+  const [showSelector, setShowSelector] = useState(true);
 
   const [options, setOptions] = useState({
     negativePrompt: "",
